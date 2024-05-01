2539bf267cf185f1916abe2535368d68ada740ad
najzeko <markom@novuscom.net>
2024-05-01 01:38:27 -0700

    add default icon to entries

--- src/components/PetEntry/PetEntry.js
+++ src/components/PetEntry/PetEntry.js
@@ -3,16 +3,19 @@ import './PetEntry.css';
 
 const PetEntry = ({ pet }) => {
   return (
-    <div className="pet-entry">
-      <div className="content">
-        <div className="pet-name">
-          <strong>{pet.name}</strong>
-        </div>
-        <div className="pet-info">
-          Animal: {pet.animal}
-        </div>
-        <div className="pet-info">
-          Color: {pet.color}
+    <div className="pet-list">
+      <div className="pet-entry">
+        <div className="content">
+          <div className="pet-name">
+            <strong>{pet.name}</strong>
+          </div>
+          <div className="pet-image">
+            <img src={`${process.env.PUBLIC_URL}/default_dog.png`} alt="Profile" />
+          </div>
+          <div className="pet-info">
+            <div>Animal: {pet.animal}</div>
+            <div>Color: {pet.color}</div>
+          </div>
         </div>
       </div>
     </div>
